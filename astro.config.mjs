d9a961866a9f1a0694264162dc640f42be96c9ce
Grzegorz Kossakowski <grzegorz.kossakowski@gmail.com>
2024-04-21 23:09:02 +0200

    The initial setup of Clerk + Astro auth
    With React integration for Clerk's <SignUp/> React component, and Vercel
    integration. Let's push it and see if it works already.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
+import clerk from "astro-clerk-auth";
+import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [clerk(), react()],
+  output: "server",
+  adapter: vercel()
+});
